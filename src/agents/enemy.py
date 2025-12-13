b453a605958b8e0b6eecc6e3b37d6cececf51c69
IamJayenn <sangadpintar@gmail.com>
2025-12-13 22:53:01 +0700

    merge

--- src/agents/enemy.py
+++ src/agents/enemy.py
@@ -26,18 +26,5 @@ class EnemyAgent:
         index = min(self.move_range - 1, len(path) - 1)
 
         next_tile = path[index]
-        # enemy_range = self.env.get_move_range(self.env.enemy_pos)
-
-        # if next_step in enemy_range:
-        #   return next_step
-
-        # best_tile = None
-        # best_dist = float("inf")
-
-        # for tile in enemy_range:
-        #   euclidean_dist = math.dist(tile, next_step)
-        #   if euclidean_dist < best_dist:
-        #     best_dist = euclidean_dist
-        #     best_tile = tile
 
         return next_tile
